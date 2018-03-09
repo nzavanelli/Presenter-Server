012cf413a569a384c3ad58d81d15c821354b0082
Felix Wohlfrom <FelixWohlfrom@users.noreply.github.com>
2018-03-09 23:07:15 +0100

    fix execution if path to presenter server contains spaces

--- src/main/res/scripts/presenter_server.sh
+++ src/main/res/scripts/presenter_server.sh
@@ -5,10 +5,10 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Start daemon in background
 echo "Starting key sender daemon."
 # Wait until the user has entered his password, but execute the daemon in background afterwards
-pkexec /bin/bash -c "${SCRIPT_DIR}/keysenderDaemon/Presenter_Server_Keysender_Daemon &"
+pkexec /bin/bash -c "\"${SCRIPT_DIR}/keysenderDaemon/Presenter_Server_Keysender_Daemon\" &"
 
 # Give the key sender some time to fire up
 sleep 0.5
 
 # Run the presenter server
-${SCRIPT_DIR}/main/Presenter_Server
+"${SCRIPT_DIR}/main/Presenter_Server"
